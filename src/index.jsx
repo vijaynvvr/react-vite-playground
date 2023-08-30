0068fb0e2ace8a69e53ce3b55cefe6e964189074
Pranav <pranav.techiegeek@gmail.com>
2023-08-30 17:07:01 +0530

    feat: update template versions

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOMClient.createRoot(document.getElementById("root")).render(<App />);
