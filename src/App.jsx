587455a23ccb8526a74637bd05f3b34aed4d7619
vijaynvvr <vijayvardhansn10@gmail.com>
2024-01-14 23:42:27 +0530

    stage2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+	const [todo, setTodo] = useState("");
 	return (
-        <div>
-            <h1>Todo App</h1>
-        </div>
+		<div>
+			<h1>Todo App</h1>
+			<input
+				placeholder="Enter todo"
+				value={todo}
+				onChange={(event) => setTodo(event.target.value)}
+			/>
+			<button>Add Todo</button>
+		</div>
 	);
 }
 
